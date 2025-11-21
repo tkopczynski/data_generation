939c8aeea283b2d862e294fae793895fff061953
Tomek Kopczynski <wonsek@gmail.com>
2025-11-21 11:23:01 +0100

    public API

--- src/data_generation/__init__.py
+++ src/data_generation/__init__.py
@@ -1,3 +1,14 @@
 """Data generation package for creating synthetic datasets."""
 
 __version__ = "0.1.0"
+
+from data_generation.api import generate, generate_with_metadata
+from data_generation.core.output_formats import SUPPORTED_FORMATS, write_dataframe
+
+__all__ = [
+    "__version__",
+    "generate",
+    "generate_with_metadata",
+    "write_dataframe",
+    "SUPPORTED_FORMATS",
+]
